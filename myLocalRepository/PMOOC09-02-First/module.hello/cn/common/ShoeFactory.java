4087a6ce61d6615e2143c82559209e47b423bfcd
wangXiaoJie <wangXiaoJie815@163.com>
2020-02-29 15:04:06 +0800

    测试

--- myLocalRepository/PMOOC09-02-First/module.hello/cn/common/ShoeFactory.java
+++ myLocalRepository/PMOOC09-02-First/module.hello/cn/common/ShoeFactory.java
@@ -6,7 +6,7 @@ import cn.serviceImpl.Warrior;
 public class ShoeFactory {
 	
 	/**
-	 * provides 第二种方法：使用一个拥有public static ,返回值为服务类或者接口，方法名为provider的类作为with（实现类）
+	 * provides 第二种方法：使用一个拥有public static ,返回值为服务类或者接口，方法名为provider的类作为with（实现类）测试
 	 * @return
 	 */
 	public static Shoe provider() {
